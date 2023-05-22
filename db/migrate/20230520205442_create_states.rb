83a1be6458d9448366d4df8f9537aca3f94a0df8
venomnert <neerthigan@gmail.com>
2023-05-22 16:09:58 -0400

    Updated existing state and area table

--- db/migrate/20230520205442_create_states.rb
+++ db/migrate/20230520205442_create_states.rb
@@ -1,8 +1,8 @@
 class CreateStates < ActiveRecord::Migration[7.0]
   def change
     create_table :states do |t|
-      t.string :state
-      t.string :country
+      t.string :state, null: false
+      t.string :country, null: false
 
       t.timestamps
     end
--- db/migrate/20230520205519_create_areas.rb
+++ db/migrate/20230520205519_create_areas.rb
@@ -1,8 +1,8 @@
 class CreateAreas < ActiveRecord::Migration[7.0]
   def change
     create_table :areas do |t|
-      t.string :city
-      t.string :airbnb_place_id
+      t.string :city, null: false
+      t.string :airbnb_place_id, null: false, unique: true
       t.references :state, null: false, foreign_key: true
 
       t.timestamps
