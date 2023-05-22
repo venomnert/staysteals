68cdc70a8555d378ea0e2dd7fae779284244aed5
venomnert <neerthigan@gmail.com>
2023-05-22 13:43:29 -0400

    Added states

---
+++ app/views/states/_state.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! state, :id, :state, :country, :created_at, :updated_at
+json.url state_url(state, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :areas
+  resources :states
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
