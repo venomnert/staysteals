3dbf7141a0d85e482a4e2505e05f01da2e781af1
venomnert <neerthigan@gmail.com>
2023-11-16 08:40:49 -0500

    ignore CA cities

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,6 +3,6 @@ class Area < ApplicationRecord
   has_many :listings
 
   def self.fetch_unique_cities
-    find_by_sql("SELECT DISTINCT city FROM areas ORDER BY city ASC")
+    find_by_sql("SELECT DISTINCT city FROM areas WHERE city != 'Prince Edward County' ORDER BY city ASC")
   end
 end
